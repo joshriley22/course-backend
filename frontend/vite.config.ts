434f3479d7ac24db0286f441c65994d258336a76
Josh Riley <arv4xc@virginia.edu>
2026-08-30 15:52:24 -0400

    Docker configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const BACKEND_URL = 'http://localhost:8000'
+const BACKEND_URL = process.env.VITE_BACKEND_URL ?? 'http://localhost:8000'
 
 const backendRoutes = ['/courses', '/classes', '/professors', '/majors']
 
