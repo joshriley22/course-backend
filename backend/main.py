82d3cbbc8c91f1b0e35a7c67330f49f949f44625
Josh Riley <arv4xc@virginia.edu>
2026-08-30 19:19:57 -0400

    Basic CORS settings

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 #from backend.logic.course_logic
 from backend.routers import course_router
 from backend.routers.course_router import router as course_router, get_co_prereqs_for_course, create_prereq_edge, get_requirements, get_next_courses
 from backend.routers.major_router import router as major_router
+from backend.routers.user_router import router as user_router
+
+origins = [
+    "http://localhost:5173",
+]
 
 app = FastAPI()
 
 app.include_router(course_router)
 app.include_router(major_router)
+app.include_router(user_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # print(get_next_courses([{"code": "CS", "number": "1110"}]))
 #create_prereq_edge("CS", "2130", "CS", "1110")
