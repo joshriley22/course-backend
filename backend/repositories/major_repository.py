bdea303222a6d2d710c4e4219d395deb70c0e301
Josh Riley <arv4xc@virginia.edu>
2026-08-31 09:13:10 -0400

    functions for creating majors

--- backend/repositories/major_repository.py
+++ backend/repositories/major_repository.py
@@ -1,6 +1,31 @@
 
 class MajorRepository:
 
+    def create_major(self, session, major_name):
+        query = """
+        MERGE (m:Major {name:$major_name})
+        """
+
+        session.run(query, major_name=major_name)
+
+    def set_major_fields(self, session, major_name, fields):
+        query = """
+        MATCH (m:Major {name:$major_name})
+        SET m.fields = $fields
+        """
+
+        session.run(query, major_name=major_name, fields=fields)
+
+    def add_course_to_major(self, session, major_name, course_code, course_number, field):
+        query="""
+        MATCH (m:Major {name:$major_name})
+        MATCH (c:Course {code:$course_code, number:$course_number})
+        MERGE (m)-[:COURSE_OF {relationship:$field}]->(c)
+        """
+
+        session.run(query, major_name=major_name, course_code=course_code, course_number=course_number, field=field)
+
+
     def get_majors(self, session):
 
         query = """
