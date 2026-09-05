aa062085dfbcaa5a287f0725f490b4832df96312
Josh Riley <arv4xc@virginia.edu>
2026-09-05 12:03:20 -0400

    Added review router

--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 #from backend.logic.course_logic
 from backend.routers import course_router
-from backend.routers.course_router import router as course_router, get_co_prereqs_for_course, create_prereq_edge, get_requirements, get_next_courses
+from backend.routers.course_router import router as course_router, get_co_prereqs_for_course, create_prereq_edge, get_next_courses
 from backend.routers.major_router import router as major_router
 from backend.routers.user_router import router as user_router
+from backend.routers.review_router import router as review_router
 
 origins = [
     "http://localhost:5173",
@@ -17,6 +18,7 @@ app = FastAPI()
 app.include_router(course_router)
 app.include_router(major_router)
 app.include_router(user_router)
+app.include_router(review_router)
 
 app.add_middleware(
     CORSMiddleware,
