c161c7048ce486dba717eee4618cb3f27ee0a1d7
Josh Riley <arv4xc@virginia.edu>
2026-09-05 09:26:33 -0400

    Refactoring into different pages

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-export function LoginPanel({ setLoggedIn }: { setLoggedIn: (loggedIn: boolean) => void }) {
+export function Login() {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
